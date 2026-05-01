a98eef8dddb5bc7713a440b83dca09e70fee0ca1
Kindal White <info@gracealivenow.org>
2026-05-01 09:56:14 -0400

    Fix process is not defined error in production

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  define: {
+    'process.env': {},
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'global': 'globalThis',
+  },
+  resolve: {
+    alias: {
+      'process': 'process/browser',
+    },
+  },
+});
